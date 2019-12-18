3435fe6b8c46fd29f9e51361b30ab42be8919483
Nicolas Herrera <nherrera@cra.com>
2019-12-18 12:58:42 -0500

    Index controller support for gestures

---
+++ Assets/VIRTUOSO/VRTK/Source/SDK/Index/SDK_IndexHand.cs
@@ -0,0 +1,106 @@
+﻿#if VRTK_DEFINE_STEAMVR_PLUGIN_2_0_0_OR_NEWER
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+#endif
+namespace VRTK
+{
+    /// <summary>
+    /// HandSDK applied to the Valve Index. The Index controllers are SteamVR based and defaults to the SteamVR Controller SDK
+    /// methods when possible.
+    /// 
+    /// Written by: Nicolas Herrera (nherrera@cra.com), 2019
+    /// </summary>
+    [SDK_Description("Index Controller", SDK_SteamVRDefines.ScriptingDefineSymbol, "OpenVR", "Standalone")]
+    public class SDK_IndexHand
+#if VRTK_DEFINE_STEAMVR_PLUGIN_2_0_0_OR_NEWER
+        : SDK_BaseHand
+#else
+        : SDK_FallbackHand
+#endif
+    {
+#if VRTK_DEFINE_STEAMVR_PLUGIN_2_0_0_OR_NEWER
+        public override void SetHandCaches(bool forceRefresh = false)
+        {
+            if (forceRefresh)
+            {
+                cachedHandControllerObject = null;
+            }
+
+            SteamVR_PlayArea steamVRPlayArea = VRTK_SharedMethods.FindEvenInactiveComponent<SteamVR_PlayArea>();
+
+            if(steamVRPlayArea != null)
+            {
+                cachedHandControllerObject = steamVRPlayArea.gameObject;
+
+                // The root for the Index is the SteamVR player area as that is the top of the SteamVR hierarchy
+                cachedRootTransform = steamVRPlayArea.transform;
+
+                cachedGestureLibrary = cachedRootTransform.GetComponentInChildren<SDK_IndexGestureLibrary>(true);
+            }
+        }
+
+        public override GameObject GetHandController()
+        {
+            GameObject hand = GetSDKManagerHandController();
+
+            if (hand == null)
+            {
+                SetHandCaches();
+
+                return cachedHandControllerObject;
+            }
+
+            return hand;
+        }
+
+        public override GameObject GetLeftHand()
+        {
+            return VRTK_SDK_Bridge.GetControllerLeftHand(true);
+        }
+
+        public override GameObject GetRightHand()
+        {
+            return VRTK_SDK_Bridge.GetControllerRightHand(true);
+        }
+
+        public override int GetHandCount()
+        {
+            int handCount = 0;
+
+            if(GetLeftHand() != null)
+            {
+                handCount++;
+            }
+
+            if(GetRightHand() != null)
+            {
+                handCount++;
+            }
+
+            return handCount;
+        }
+
+        public override void ProcessFixedUpdate(Dictionary<string, object> options)
+        {
+            // Noop - Implement for base class
+        }
+
+        public override void ProcessUpdate(Dictionary<string, object> options)
+        {
+            // Noop - Implement for base class
+        }
+
+        public override Transform GetRootTransform()
+        {
+            if(cachedRootTransform == null)
+            {
+                SetHandCaches();
+            }
+
+            return cachedRootTransform;
+        }
+#endif
+    }
+
+}
